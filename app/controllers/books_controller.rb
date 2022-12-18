4890aa53a0a8ad741e481209c96fb979fa33e1c6
EC2 Default User <ec2-user@ip-172-31-34-129.ap-northeast-1.compute.internal>
2022-12-18 14:23:35 +0000

    [add]create機能エラー対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def new
+    @bookers=Bookers.index
   end
 
+  def create
+    bookers = Bookers.index(books_params)
+    
+    books.save
+    
+    redirect_to '/show'
+  end
+  
   def index
+
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def books_params
+    params.require(:bookers).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  post'books' => 'books#create'
   get 'books'=>'books#index'
   get 'books/show'
   get 'books/edit'
