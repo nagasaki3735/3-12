e0a84ae7d886ca081dc2ca2dad021c2057356ea4
EC2 Default User <ec2-user@ip-172-31-34-129.ap-northeast-1.compute.internal>
2022-12-21 14:45:50 +0000

    [add]index.html.erbとcontroller.rbwo 手直し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,43 @@
 class BooksController < ApplicationController
-  def new
-    
-  end
   
   def create
-    @book = Book.new(book_params)
-    
-    @book.save
-    
-    redirect_to '/show'
+	   @book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book)
+  	else
+  		render :new
+  	end
   end
   
   def index
-    @book = Book.new
-    
-    @book = Book.all
+   @books = Book.all
+  	@book = Book.new(book_params)
 
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to book_path(@book)
+  end
+  
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	redirect_to books_path
   end
 
   private
+  def
   def book_params
-    params.require(:book).permit(:title, :body)
+  	params.require(:book).permit(:title, :body)
   end
-e
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  post'books' => 'books#create'
-  get 'books'=>'books#index'
-  get 'show'=>'books#show'
-  get 'books/edit'
-  get  root to: 'homes#top'
+  root :to => 'homes#top'
+	get 'books' =>'books#index'
+	post 'books' => 'books#create'
+	get 'books/:id' => 'books#show', as: 'book'
+	get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+	patch 'books/:id' => 'books#update', as: 'update_book'
+	put 'books/:id' => 'books#update'
+	delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
